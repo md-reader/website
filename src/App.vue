99561b8b1f0c4ec7ccb85fbfc533c2bcd5fd2f80
Bener <benjavan@163.com>
2023-02-13 14:15:34 +0800

    feat: add nav-link component

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,29 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from "vue-router";
 import { useBackgroundMask } from "./shared";
+import NavLink from "./components/NavLink.vue";
 useBackgroundMask();
 </script>
 
 <template>
   <header>
     <nav>
-      <RouterLink draggable="false" to="/">Home</RouterLink>
-      <RouterLink draggable="false" to="/about">About</RouterLink>
+      <RouterLink
+        custom
+        draggable="false"
+        to="/"
+        v-slot="{ navigate, isExactActive }"
+      >
+        <NavLink :class="{ isExactActive }" @click="navigate">Home</NavLink>
+      </RouterLink>
+      <RouterLink
+        custom
+        draggable="false"
+        to="/about"
+        v-slot="{ navigate, isExactActive }"
+      >
+        <NavLink :class="{ isExactActive }" @click="navigate">About</NavLink>
+      </RouterLink>
     </nav>
   </header>
 
@@ -24,53 +39,7 @@ nav {
   font-size: 18px;
   text-align: center;
   margin-top: 1rem;
-  color: var(--color-heading);
   user-select: none;
-}
-
-nav a {
-  position: relative;
-  display: inline-block;
-  padding: 0 1.2rem;
-  font-weight: bold;
-  color: #607cd2;
-  transition: transform 0.3s;
-}
-
-nav a::before {
-  content: "";
-  width: 100%;
-  height: 100%;
-  left: 0;
-  bottom: -50%;
-  position: absolute;
-  border-radius: 10px;
-  opacity: 0.25;
-  transform: scaleY(0.14) scaleX(0);
-  transition: opacity 0.3s, bottom 0.08s ease-in-out, transform 0.3s;
-}
-
-nav a:last-of-type {
-  margin-left: 0.4rem;
-}
-
-nav a:hover::before {
-  opacity: 0.3;
-  transform: scaleY(0.14) scaleX(0.3);
-  background: currentColor;
-}
-nav a:active {
-  transform: scale(0.95);
-}
-
-nav a:active::before {
-  opacity: 0.35;
-}
-
-nav a:active::before,
-nav a.router-link-exact-active::before {
-  bottom: 0;
-  transform: none;
-  background: currentColor;
+  color: var(--color-heading);
 }
 </style>
---
+++ src/components/NavLink.vue
@@ -0,0 +1,56 @@
+<template>
+  <a v-bind="$attrs"><slot></slot></a>
+</template>
+
+<style scoped>
+a {
+  position: relative;
+  display: inline-block;
+  padding: 0 1.2rem;
+  font-weight: bold;
+  color: #607cd2;
+  transition: transform 0.3s;
+}
+
+a::before {
+  content: "";
+  width: 100%;
+  height: 100%;
+  left: 0;
+  bottom: -50%;
+  position: absolute;
+  border-radius: 10px;
+  opacity: 0.25;
+  transform: scaleY(0.14) scaleX(0);
+  transition: opacity 0.3s, bottom 0.2s ease-in-out, transform 0.2s,
+    background 0.05s;
+  background: transparent;
+}
+
+a:last-of-type {
+  margin-left: 0.4rem;
+}
+
+a:hover::before {
+  opacity: 0.3;
+  transform: scaleY(0.14) scaleX(0.3);
+  background: currentColor;
+}
+
+a:active {
+  transform: scale(0.95);
+}
+
+a:active::before {
+  opacity: 0.35;
+}
+
+a:active::before,
+a.isExactActive::before {
+  bottom: 0;
+  transform: scale(1);
+  transition: opacity 0.3s, bottom 0.1s ease-in-out, transform 0.3s,
+    background 0.05s;
+  background: currentColor;
+}
+</style>
