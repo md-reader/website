5c4576c1dac976ef217cc9b7c1d21c0f62cfb021
Bener <benjavan@163.com>
2024-12-24 22:51:44 +0800

    feat: add bluesky link

--- components/header-actions.vue
+++ components/header-actions.vue
@@ -1,9 +1,12 @@
 <template>
   <div class="flex justify-end">
+    <UButton v-show="colorMode.unknown" icon="i-mdi-loading" :color="null" variant="link" class="animate-spin" />
     <UButton v-show="!colorMode.unknown && isDark" icon="i-heroicons-moon-20-solid" :color="null" variant="link"
       aria-label="Theme" @click="isDark = !isDark" />
     <UButton v-show="!colorMode.unknown && !isDark" icon="i-heroicons-sun-20-solid" :color="null" variant="link"
       aria-label="Theme" @click="isDark = !isDark" />
+    <UButton icon="i-ri-bluesky-fill" :color="null" variant="link" aria-label="Bluesky"
+      to="https://bsky.app/profile/markdownreader.bsky.social" target="_blank_Bluesky" />
     <UButton icon="i-ri-twitter-x-fill" :color="null" variant="link" aria-label="Twitter" to="https://x.com/BenerHero"
       target="_blank_Twitter" />
     <UButton icon="i-mdi-github" :color="null" variant="link" aria-label="GitHub"
